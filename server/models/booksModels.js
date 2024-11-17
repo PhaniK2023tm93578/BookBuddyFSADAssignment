33753c765f71d03a25197b72521af9e317956480
PhaniK00 <2023tm93578@wilp.bits-pilani.ac.in>
2024-11-17 22:08:32 +0530

    Cleanup comments

--- server/models/booksModels.js
+++ server/models/booksModels.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg');
 
-// add elephant SQL url here. Queries to create tables are below :) 
 const PG_URI = 'postgres://YourUserName:YourPassword@localhost:5432/YourDatabase';
 
 // create a new pool here using the connection string above
 const pool = new Pool({
   connectionString: PG_URI,
 });
-
 
 module.exports = {
   query: (text, params, callback) => {
